68d7d8433158cd6de05b9bad5eb3ebb2cc50d286
hirantha <sahanhirantha@gmail.com>
2020-03-08 13:58:11 +0530

    migrate to androidx

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hiruu/Documents/Private Projects/SCoRe-Mobile-App"
+export "FLUTTER_APPLICATION_PATH=/home/hirantha/Documents/private projects/scorelab/SCoRe-Mobile-App"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
