b7d5ef121fba21c5d6af674cf498b0b8dd0e6e62
hirantha <sahanhirantha@gmail.com>
2020-03-21 00:37:18 +0530

    configured with dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:score_app/screens/splash_screen.dart';
+import 'package:score_app/states/config_state.dart';
 
-void main() => runApp(ScoreApp());
+void main() {
+  Provider.debugCheckInvalidValueType = null;
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ConfigState>.value(
+          value: ConfigState(),
+        ),
+      ],
+      child: ScoreApp(),
+    ),
+  );
+}
 
 class ScoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ConfigState configState = Provider.of<ConfigState>(context);
     return MaterialApp(
       title: 'Score Mobile',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        brightness: Brightness.dark
+        brightness: configState.darkTheme ? Brightness.dark : Brightness.light,
       ),
       home: SplashScreen(),
     );
