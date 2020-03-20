fcd07b31a61965c038d2a6613bcaea4c7fe199ac
hirantha <sahanhirantha@gmail.com>
2020-03-21 01:03:43 +0530

    button added to toggle dark theme

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:score_app/states/config_state.dart';
 
 class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ConfigState configState = Provider.of<ConfigState>(context);
     return Container(
-      
+      child: Column(
+        children: <Widget>[
+          configState.darkTheme
+              ? ListTile(
+                  leading: Icon(FontAwesomeIcons.lightbulb),
+                  title: Text("Turn off dark mode"),
+                  onTap: () {
+                    configState.toggleDarkTheme();
+                  },
+                )
+              : ListTile(
+                  leading: Icon(FontAwesomeIcons.solidLightbulb),
+                  title: Text("Turn on dark mode"),
+                  onTap: () {
+                    configState.toggleDarkTheme();
+                  },
+                ),
+        ],
+      ),
     );
   }
 }
